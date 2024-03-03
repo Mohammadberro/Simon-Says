91b870b2e28ac4a0082c81f85fb2e3d6402627d8
Mohammadberro <ikerberro1@gmail.com>
2024-03-03 14:57:34 +0200

    Initializing Variables

---
+++ js/game.js
@@ -0,0 +1,13 @@
+let sequence = [];
+let playerSequence = [];
+let playerTurn;
+let compTurn;
+let flash;
+const playButton = document.getElementById('play');
+const blueTone = new Audio("../sounds/blue.mp3");
+const greenTone = new Audio("../sounds/green.mp3");
+const redTone = new Audio("../sounds/red.mp3");
+const yellowTone = new Audio("../sounds/yellow.mp3");
+const wrongTone = new Audio("../sounds/wrong.mp3");
+const gameOverTone = new Audio("../sounds/game-over.mp3");
+const gameWinTone = new Audio("../sounds/game-win.mp3");
