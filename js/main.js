927c6f122ea2f15dd8a5f8fcbf6b77e277f29be8
Mohammadberro <ikerberro1@gmail.com>
2024-03-03 17:38:55 +0200

    Animating Tiles

--- js/main.js
+++ js/main.js
@@ -12,15 +12,13 @@ const tileBlue = document.getElementsByClassName("tile blue");
 // Lists and Booleans
 let sequence = [];
 let playerSequence = [];
-let playerTurn=false;
-let compTurn=true;
-let flash;
+let playerLevel = level.innerText;
 
 // Audio Tones
-const blueTone = new Audio("../sounds/blue.mp3");
-const greenTone = new Audio("../sounds/green.mp3");
-const redTone = new Audio("../sounds/red.mp3");
-const yellowTone = new Audio("../sounds/yellow.mp3");
+const toneBlue = new Audio("../sounds/blue.mp3");
+const toneGreen = new Audio("../sounds/green.mp3");
+const toneRed = new Audio("../sounds/red.mp3");
+const toneYellow = new Audio("../sounds/yellow.mp3");
 const wrongTone = new Audio("../sounds/wrong.mp3");
 const gameOverTone = new Audio("../sounds/game-over.wav");
 const gameWinTone = new Audio("../sounds/game-win.wav");
@@ -28,8 +26,9 @@ const gameWinTone = new Audio("../sounds/game-win.wav");
 // Functions
 function activatePlayButton(){
     playButton.addEventListener('click', () => {
-        play();
+        playerLevel=0;
         sequence = [];
+        play();
         })
 }
 
@@ -49,19 +48,44 @@ function makeClickable(){
 
 function play(){
     createRestartButton()
-    let level = 0;
-    makeUnclickable;
-    generateSecquence();
+    makeUnclickable();
+    generateSequence();
+    computerSequence(playerLevel);
     makeClickable();
 }
 
-function generateSecquence(){
+function generateSequence(){
     for (let i=0; i < 12 ; i++){
         sequence.push(tiles[Math.floor(Math.random() * tiles.length)])
     }
-    console.log(sequence);
+
 }
 
 function createRestartButton(){
     playButton.innerHTML ="Restart";
+}
+
+function computerSequence(level){
+    for(let i=0; i<=level; i++){
+        animateTile(sequence[i]);
+    }
+}
+
+function animateTile(tile){
+    if (tile == tileBlue){
+        tileBlue.classList.remove("inactive");
+        toneBlue.play();
+    }
+    else if (tile == tileGreen){
+        tileBlue.classList.remove("inactive");
+        toneGreen.play();
+    }
+    else if (tile == tileRed){
+        tileRed.classList.remove("inactive");
+        toneRed.play();
+    }
+    else if (tile == tileYellow){
+        tileYellow.classList.remove("inactive");
+        toneYellow.play();
+    }
 }
